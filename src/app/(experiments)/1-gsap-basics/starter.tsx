98005c757463093709f0056c79316209fef408d1
Daria Borisiak <daria.borisiak@gmail.com>
2026-09-15 12:01:07 +0200

    Use gsap context

--- src/app/(experiments)/1-gsap-basics/starter.tsx
+++ src/app/(experiments)/1-gsap-basics/starter.tsx
@@ -1,26 +1,28 @@
 "use client";
 
-import {useEffect} from "react";
+import {useEffect, useRef} from "react";
 import gsap from 'gsap'
 
 export default function Page() {
+    const containerRef = useRef(null)
+
     useEffect(() => {
-        const tween = gsap.to('.title', {
-            x: -200,
-            duration: 10,
-            onUpdate: () => {
-                console.log('update')
-            }
-        })
+        const ctx = gsap.context(() => {
+            gsap.to('.title', {
+                x: 200,
+                duration: 1,
+                onUpdate: () => console.log('update')
+            })
+        }, containerRef)
 
         return () => {
-            tween.revert()
+            //tween.revert()
         }
     }, [])
 
   return (
     <div className="bg-blue-300 text-black">
-      <div className="flex h-screen items-end justify-left overflow-hidden">
+      <div ref={containerRef} className="flex h-screen items-end justify-left overflow-hidden">
         <h1 className="title font-black text-[min(20rem,30vw)] leading-none pb-[0.1em] text-left">
           GSAP
           <br />
