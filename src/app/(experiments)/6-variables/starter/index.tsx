3d37b55f2dc3683a0f00e8a5347a4266f420c7ed
Daria Borisiak <daria.borisiak@gmail.com>
2026-09-15 21:07:20 +0200

    Animate font weight on mouse move

--- src/app/(experiments)/6-variables/starter/index.tsx
+++ src/app/(experiments)/6-variables/starter/index.tsx
@@ -1,9 +1,43 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import React from 'react';
 import s from "./styles.module.css";
+import {useEffect, useState} from "react";
+import {distance} from "@/lib/math";
 
 export default function Page() {
+    const [distanceValue, setDistanceValue] = useState(0);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    window.addEventListener(
+        'mousemove',
+        (event) => {
+          const mouseX = event.clientX;
+          const mouseY = event.clientY;
+
+          const screenW = window.innerWidth;
+          const screenH = window.innerHeight;
+
+          const centerX = screenW / 2;
+          const centerY = screenH / 2;
+
+          const d = distance(mouseX, mouseY, centerX, centerY)
+
+          const maxDistance = distance(0, 0, centerX, centerY);
+
+            setDistanceValue(d / maxDistance);
+        },
+        {
+          signal: controller.signal,
+        }
+    )
+
+    return () => controller.abort();
+  }, [])
+
   return (
     <div
       className={cn(
@@ -16,6 +50,9 @@ export default function Page() {
           "uppercase text-[10vh] leading-none relative",
           s["title"]
         )}
+        style={{
+            '--distance': distanceValue,
+        } as React.CSSProperties}
       >
         Variables
       </h1>
