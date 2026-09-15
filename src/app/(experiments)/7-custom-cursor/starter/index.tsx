609250d6636e02f989efe1febfe6dadcd1a916aa
Daria Borisiak <daria.borisiak@gmail.com>
2026-09-15 22:04:25 +0200

    Snap cursor

--- src/app/(experiments)/7-custom-cursor/starter/index.tsx
+++ src/app/(experiments)/7-custom-cursor/starter/index.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import s from "./styles.module.css";
-import {useEffect, useRef} from "react";
+import {PointerEventHandler, useCallback, useEffect, useRef, useState} from "react";
 import {lerp} from "@/lib/math";
 import gsap from 'gsap';
 
 export default function Page() {
+    const [snap, setSnap] = useState<null | {
+        x: number;
+        y: number;
+        width: number;
+        height: number;
+    }>(null);
+
     const mouseRef = useRef<HTMLDivElement>(null);
 
     const cursorPosRef = useRef({
@@ -20,17 +27,32 @@ export default function Page() {
 
     useEffect(() => {
         const callback: gsap.TickerCallback = (time, deltaTime) => {
-            cursorPosRef.current.x = lerp(
-                cursorPosRef.current.x,
-                cursorTargetRef.current.x,
-                deltaTime * 0.1
-            );
+            if (snap) {
+                cursorPosRef.current.x = lerp(
+                    cursorPosRef.current.x,
+                    snap.x,
+                    deltaTime * 0.01
+                );
 
-            cursorPosRef.current.y = lerp(
-                cursorPosRef.current.y,
-                cursorTargetRef.current.y,
-                deltaTime * 0.1
-            );
+                cursorPosRef.current.y = lerp(
+                    cursorPosRef.current.y,
+                    snap.y,
+                    deltaTime * 0.01
+                );
+
+            } else {
+                cursorPosRef.current.x = lerp(
+                    cursorPosRef.current.x,
+                    cursorTargetRef.current.x,
+                    deltaTime * 0.01
+                );
+
+                cursorPosRef.current.y = lerp(
+                    cursorPosRef.current.y,
+                    cursorTargetRef.current.y,
+                    deltaTime * 0.01
+                );
+            }
 
             if (mouseRef.current) {
                 mouseRef.current.style.setProperty('--x', cursorPosRef.current.x.toString());
@@ -41,7 +63,7 @@ export default function Page() {
         const cb = gsap.ticker.add(callback);
 
         return () => gsap.ticker.remove(cb);
-    }, [])
+    }, [snap])
 
     useEffect(() => {
         const controller = new AbortController();
@@ -62,12 +84,35 @@ export default function Page() {
         return () => controller.abort()
     }, [])
 
+    const onPointerEnter = useCallback<
+        PointerEventHandler<HTMLHeadingElement>
+        >((event) => {
+            const rect = event.currentTarget.getClientRects()[0];
+
+
+        setSnap({
+            x: window.innerWidth / 2,
+            y: window.innerHeight / 2,
+            width: rect.width + 20,
+            height: rect.height + 20,
+        })
+    }, []);
+    const onPointerLeave = useCallback(() => {
+        setSnap(null);
+    }, []);
+
   return (
     <div className="w-screen h-screen bg-black text-green-400 flex items-center justify-center">
-      <h1 className="uppercase text-[10vh] leading-none relative cursor-default pl-[0.1em] opacity-60 hover:opacity-100">
+      <h1
+          onPointerEnter={onPointerEnter}
+          onPointerLeave={onPointerLeave}
+          className="uppercase text-[10vh] leading-none relative cursor-default pl-[0.1em] opacity-60 hover:opacity-100">
         Start
       </h1>
-      <div className={s.cursor} ref={mouseRef} />
+      <div className={s.cursor} ref={mouseRef} style={{
+          '--w': snap ? snap.width+'px' : undefined,
+          '--h': snap ? snap.height+'px' : undefined,
+      }}/>
     </div>
   );
 }
