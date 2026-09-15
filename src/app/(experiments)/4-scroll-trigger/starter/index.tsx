bbe003ac1600cc170fabb6793c7e808442032a10
Daria Borisiak <daria.borisiak@gmail.com>
2026-09-15 17:32:26 +0200

    Animate scroll

--- src/app/(experiments)/4-scroll-trigger/starter/index.tsx
+++ src/app/(experiments)/4-scroll-trigger/starter/index.tsx
@@ -6,6 +6,10 @@ import { TitleSection } from "./title";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
+import { ScrollTrigger, DrawSVGPlugin } from 'gsap/all'
+gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(DrawSVGPlugin)
+
 export default function Page() {
   return (
     <>
@@ -21,12 +25,25 @@ function DescriptionSection() {
 
   useGSAP(
     () => {
-      const tl = gsap.timeline();
+      const tl = gsap.timeline({
+          scrollTrigger: {
+              trigger: 'h2',
+              start: 'top center',
+              end: 'bottom center',
+              toggleActions: 'play reverse play reverse',
+              markers: true,
+          }
+      });
 
       tl.from("h2", {
         opacity: 0,
         duration: 1,
       });
+
+      tl.from('path', {
+          drawSVG: 0,
+          stagger: 1,
+      })
     },
     {
       scope: containerRef,
