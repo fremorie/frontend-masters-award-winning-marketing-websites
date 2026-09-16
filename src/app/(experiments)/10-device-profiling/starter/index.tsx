8f2da6363cada93c781bbb7951fec3728b0f7673
Daria Borisiak <daria.borisiak@gmail.com>
2026-09-16 12:35:28 +0200

    Detect GPU

--- src/app/(experiments)/10-device-profiling/starter/index.tsx
+++ src/app/(experiments)/10-device-profiling/starter/index.tsx
@@ -1,7 +1,38 @@
 "use client";
 
 import { ShaderEffect } from "./logo-webgl";
+import {StaticVersion} from "./logo-static";
+import {useEffect, useState} from "react";
+import { getGPUTier } from "detect-gpu";
+import { getSelectorsByUserAgent } from 'react-device-detect'
+import {type DeviceType} from "@/types";
 
 export default function Page() {
-  return <ShaderEffect />;
+  const shouldUseGl = useShouldRenderGl()
+
+  return shouldUseGl ? <ShaderEffect /> : <StaticVersion />
+}
+
+function useShouldRenderGl() {
+  const [gpuTier, setGpuTier] = useState<number | null>(null)
+  const [deviceInfo, setDeviceInfo] = useState<DeviceType | null>(null)
+
+  useEffect(() => {
+    const result: DeviceType = getSelectorsByUserAgent(window.navigator.userAgent);
+    setDeviceInfo(result);
+
+    getGPUTier().then(result => {
+      setGpuTier(result.tier)
+    })
+  }, [])
+
+  if (typeof gpuTier === 'number' && gpuTier < 2) {
+    return false;
+  }
+
+  if (deviceInfo && deviceInfo.isSafari) {
+    return false;
+  }
+
+  return true;
 }
